d91a1ab4f0e5103b0b647267dc4b64f13113116c
abhkpr <abhkpr@users.noreply.github.com>
2026-03-02 18:14:07 +0000

    deploy: 1200d5e858e792c8eea6c42fc7020da79e3954e1

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-aac1e9d9.js',
+                path_to_root + 'searchindex-cb4441e3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
